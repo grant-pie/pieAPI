27b42589ca3e4a1c2d5782ca3619bccf4787bafb
grant-pie <grant.pieterse1@gmail.com>
2025-02-25 14:20:28 +0200

    Added contact controller logic

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { ContactDto } from './contact.dto';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+  constructor(private readonly contactService: ContactService) {}
+
+  @Post()
+  async handleContactForm(@Body() contactDto: ContactDto) {
+    return await this.contactService.sendEmail(contactDto);
+  }
+}
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ContactDto } from './contact.dto';
 
 @Injectable()
-export class ContactService {}
+export class ContactService {
+  async sendEmail(contactDto: ContactDto) {
+    // Here, we will later add the logic to send an email
+    return { message: 'Your message has been received!', data: contactDto };
+  }
+}
