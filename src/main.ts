c2db8fab51892efb6b4156512d4391b17ee70e01
grant-pie <grant.pieterse1@gmail.com>
2025-03-01 18:26:50 +0200

    Added SC files

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Enable global validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
-
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.enableCors({
     origin: process.env.DOMAIN,
   });
